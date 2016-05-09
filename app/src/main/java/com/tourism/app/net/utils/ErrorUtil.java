d0d05c07cc80dc3ef991dea3bc4d52e54cd12c02
jiang.zeyu <jiang.zeyu@etao.cn>
2016-05-09 21:46:30 +0800

    上传文件

--- app/src/main/java/com/tourism/app/net/utils/ErrorUtil.java
+++
@@ -1,7 +0,0 @@
-package com.tourism.app.net.utils;
-
-public class ErrorUtil {
-	public static String errorJson(String resultCode,String message){
-		return "{\"success\":\""+resultCode+"\",\"message\":\""+message+"\"}";
-	}
-}
